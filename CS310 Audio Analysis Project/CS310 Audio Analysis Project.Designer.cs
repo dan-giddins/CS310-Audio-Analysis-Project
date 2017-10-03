6a46e3d2a5875f6397431b566e5661d67bcfdaf6
Protocat112 <protocat112@gmail.com>
2017-10-03 23:08:49 +0100

    Allows for input device selection

--- CS310 Audio Analysis Project/CS310 Audio Analysis Project.Designer.cs
+++ CS310 Audio Analysis Project/CS310 Audio Analysis Project.Designer.cs
@@ -29,25 +29,18 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.lblOutput = new System.Windows.Forms.Label();
             this.btnRecord = new System.Windows.Forms.Button();
             this.tmrLabel = new System.Windows.Forms.Timer(this.components);
+            this.boxDevices = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lblDevices = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // lblOutput
-            // 
-            this.lblOutput.AutoSize = true;
-            this.lblOutput.Location = new System.Drawing.Point(94, 18);
-            this.lblOutput.Name = "lblOutput";
-            this.lblOutput.Size = new System.Drawing.Size(49, 13);
-            this.lblOutput.TabIndex = 0;
-            this.lblOutput.Text = "lblOutput";
             // 
             // btnRecord
             // 
-            this.btnRecord.Location = new System.Drawing.Point(13, 13);
+            this.btnRecord.Location = new System.Drawing.Point(12, 40);
             this.btnRecord.Name = "btnRecord";
-            this.btnRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnRecord.Size = new System.Drawing.Size(532, 23);
             this.btnRecord.TabIndex = 1;
             this.btnRecord.Text = "Record";
             this.btnRecord.UseVisualStyleBackColor = true;
@@ -55,15 +48,46 @@
             // 
             // tmrLabel
             // 
+            this.tmrLabel.Interval = 50;
             this.tmrLabel.Tick += new System.EventHandler(this.tmrLabel_Tick);
+            // 
+            // boxDevices
+            // 
+            this.boxDevices.FormattingEnabled = true;
+            this.boxDevices.Location = new System.Drawing.Point(89, 13);
+            this.boxDevices.Name = "boxDevices";
+            this.boxDevices.Size = new System.Drawing.Size(300, 21);
+            this.boxDevices.TabIndex = 2;
+            this.boxDevices.SelectedIndexChanged += new System.EventHandler(this.boxDevices_SelectedIndexChanged);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(394, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(150, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh Device List";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // lblDevices
+            // 
+            this.lblDevices.AutoSize = true;
+            this.lblDevices.Location = new System.Drawing.Point(12, 17);
+            this.lblDevices.Name = "lblDevices";
+            this.lblDevices.Size = new System.Drawing.Size(71, 13);
+            this.lblDevices.TabIndex = 4;
+            this.lblDevices.Text = "Input Device:";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(556, 536);
+            this.Controls.Add(this.lblDevices);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.boxDevices);
             this.Controls.Add(this.btnRecord);
-            this.Controls.Add(this.lblOutput);
             this.Name = "MainForm";
             this.Text = "CS310 Audio Analysis Project";
             this.ResumeLayout(false);
@@ -72,10 +96,11 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblOutput;
         private System.Windows.Forms.Button btnRecord;
         private System.Windows.Forms.Timer tmrLabel;
+        private System.Windows.Forms.ComboBox boxDevices;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label lblDevices;
     }
 }
 
--- CS310 Audio Analysis Project/CS310 Audio Analysis Project.cs
+++ CS310 Audio Analysis Project/CS310 Audio Analysis Project.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using NAudio.Wave;
 
@@ -15,54 +8,82 @@ namespace CS310_Audio_Analysis_Project
     {
         public WaveIn waveIn;
         public BufferedWaveProvider bufferedWaveProvider;
+        private int currentDevice;
         private const int SAMPLE_RATE = 44100;
-        private const int BUFFER_SIZE = 4096;
+        private const int BUFFER_SIZE = 16;
 
         public MainForm()
         {
             InitializeComponent();
+            updateAudioDevices();
         }
 
         private void btnRecord_Click(object sender, EventArgs e)
         {
-            //get all avaible audio devices
-            int deviceCount = WaveIn.DeviceCount;
-            Console.Out.WriteLine("Device Count: {0}.", deviceCount);
+            tmrLabel.Enabled = false;
             //create a WaveIn object
             WaveIn waveIn = new WaveIn();
-            waveIn.DeviceNumber = 0;
+            waveIn.DeviceNumber = currentDevice;
             waveIn.WaveFormat = new WaveFormat(SAMPLE_RATE, 1);
             //create a wave buffer and start the recording
-            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(wi_DataAvailable);
+            waveIn.DataAvailable += new EventHandler<WaveInEventArgs>(waveInDataAvailable);
             bufferedWaveProvider = new BufferedWaveProvider(waveIn.WaveFormat);
             bufferedWaveProvider.BufferLength = BUFFER_SIZE * 2;
             bufferedWaveProvider.DiscardOnBufferOverflow = true;
             waveIn.StartRecording();
+            tmrLabel.Enabled = true;
+        }
+
+        private void updateAudioDevices()
+        {
+            //get all avaible audio devices
+            int deviceCount = WaveIn.DeviceCount;
+            Console.Out.WriteLine("Device Count: " + deviceCount);
+            boxDevices.Items.Clear();
+            for (int i = 0; i < deviceCount; i++)
+            {
+                WaveInCapabilities capabilities = WaveIn.GetCapabilities(i);
+                boxDevices.Items.Add(i + ": " + capabilities.ProductName + ", Channels: " + capabilities.Channels);
+            }
         }
 
         //add data to the audio recording buffer
-        private void wi_DataAvailable(object sender, WaveInEventArgs e)
+        private void waveInDataAvailable(object sender, WaveInEventArgs e)
         {
             bufferedWaveProvider.AddSamples(e.Buffer, 0, e.BytesRecorded);
         }
 
         private void update_label()
         {
+            tmrLabel.Enabled = false;
             //read the bytes from the stream
             const int BIT_DEPTH = 16;
             const int BYTES_PER_SAMPLE = BIT_DEPTH / 8;
-            int[] values = new int[8];
+            int[] values = new int[BUFFER_SIZE / BYTES_PER_SAMPLE];
             int frameSize = values.Length * BYTES_PER_SAMPLE;
             byte[] frameArray = new byte[frameSize];
-            tmrLabel.Enabled = false;
             bufferedWaveProvider.Read(frameArray, 0, frameArray.Length);
-
+            for (int i=0; i<values.Length; i++)
+            {
+                values[i] = (frameArray[i * 2 + 1] << 8) | frameArray[i * 2];
+            }
             tmrLabel.Enabled = true;
         }
 
         private void tmrLabel_Tick(object sender, EventArgs e)
         {
             update_label();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            updateAudioDevices();
+        }
+
+        private void boxDevices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentDevice = boxDevices.SelectedIndex;
+            Console.Out.WriteLine("Selected device: " + currentDevice);
         }
     }
 }
